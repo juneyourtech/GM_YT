08c3d25676f164875bc85850b94b3cb718a05940
MartR <martrootamm@users.noreply.github.com>
2016-09-24 11:05:11 +0300

    Update GPlus.user.js

--- GPlus.user.js
+++ GPlus.user.js
@@ -2,8 +2,9 @@
 // @name        GPlus
 // @namespace   GPlus
 // @description Google Plus font fix for long texts (Windows XP)
+// @updateURL https://github.com/martrootamm/GM_YT/raw/master/GPlus.user.js
 // @include     https://plus.google.com/*
-// @version     1
+// @version     0.1.0
 // @grant       GM_addStyle
 // ==/UserScript==
 // Notes:
