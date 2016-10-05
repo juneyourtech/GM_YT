8ca4c63ea4d96c7bc8884deafa480311a4d1b23e
MartR <martrootamm@users.noreply.github.com>
2016-10-06 00:45:16 +0300

    Update YouTube_lower-res.user.js

--- YouTube_lower-res.user.js
+++ YouTube_lower-res.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          YouTube
 // @namespace     YT_lowerres
-// @description  YouTube site layout improvements for displays with smaller resolutions.
+// @description  YouTube site layout improvements for displays with smaller resolutions (1024x768, 800x600).
 // @updateURL https://github.com/martrootamm/GM_YT/raw/master/YouTube_lower-res.user.js
 // @version 0.8.0.1
 // @include       *.youtube.com/*
